54dab3b44ddf493b4f98de0bfa0bbde346f8e913
James Hwang <jbh7711@gmail.com>
2023-02-13 19:29:23 +0900

    refresh UserProfileDialog

--- src/Fims.Client.Shared/Pages/Account/UserProfileDialog.razor.cs
+++ src/Fims.Client.Shared/Pages/Account/UserProfileDialog.razor.cs
@@ -64,7 +64,7 @@ namespace Fims.Client.Shared.Pages.Account
 
         protected override async Task OnInitializedAsync()
         {
-            await this.LoadDataAsync();
+            //await this.LoadDataAsync();
             await base.OnInitializedAsync();
         }
 
@@ -80,7 +80,7 @@ namespace Fims.Client.Shared.Pages.Account
             if (firstRender)
             {
                 await this.LoadDataAsync();
-                StateHasChanged();
+                //StateHasChanged();
             }
         }
 
@@ -142,7 +142,8 @@ namespace Fims.Client.Shared.Pages.Account
             //this.UserProfileModel.EnglishName = user.GetEnglishName();
             this.CurrentUserProfileModel.Role = user.GetUserRole();
 
-            StateHasChanged();
+            //StateHasChanged();
+            this.NavigationManager.NavigateTo("/account/userprofiledialog", forceLoad: false);
         }
     }
 }
